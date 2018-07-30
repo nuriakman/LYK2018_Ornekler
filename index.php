b362e9ed007b53c7993e7ddc7a7047a8e87fcb78
Nuri Akman <nuriakman@gmail.com>
2018-07-30 16:21:47 +0300

    Üzerinde çalışılacak dosyayı seçebilme örneği eklendi

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
       <li><a href='dosya_yukle.php' target="_blank">PHP ile dosya yükleme örneği</a></li>
       <li><a href='text_dosyaya_ekleme.php' target="_blank">Text dosya sonuna metin ekleme örneği</a></li>
       <li><a href='text_dosya_duzenleme.php' target="_blank">PHP ile dosya düzenleme örneği</a></li>
+      <li><a href='text_dosya_duzenleme_gelismis.php' target="_blank">PHP ile dosya düzenleme örneği (Gelişmiş)</a></li>
     </ul>
   </body>
 </html>
