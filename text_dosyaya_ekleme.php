c4b7da3fb3f839e2ec128532ba675915a8a85f4c
Nuri Akman <nuriakman@gmail.com>
2018-07-30 15:41:48 +0300

    Text dosyaya veri ekleme örneği yapıldı

---
+++ text_dosyaya_ekleme.php
@@ -0,0 +1,51 @@
+<?php
+
+  $Dosya = "textdosya.txt";
+
+  if(isset($_GET["ILAVE"])) {
+    $DosyaninMevcutIcerigi = file_get_contents($Dosya);
+    $EklenecekIfade = $_GET["ILAVE"];
+    $DosyaninMevcutIcerigi .= "\n" . $EklenecekIfade;
+    file_put_contents($Dosya, $DosyaninMevcutIcerigi);
+    echo "<h3>Başarılı!</h3>";
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="TR">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <h1>Text Dosyaya Metin Ekleme Örneği</h1>
+
+  <form method="get">
+
+    Dosyaya Eklenecek Mesaj:<br>
+    <input type="text" name="ILAVE"><br><br>
+    <input type="submit" value="Gönder">
+
+  </form>
+
+  <h1>Dosyanın İçeriği</h1>
+
+  <?php
+    $DosyaIcerigi = file_get_contents($Dosya);
+    echo nl2br($DosyaIcerigi);
+  ?>
+
+
+
+
+
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+
+  </body>
+</html>
